d9270a89bac8a58e305a15b57d0fe007e295683f
lonewolfyx <877085893@qq.com>
2025-12-31 11:03:34 +0800

    refactor(config): Parameter handling of the optimized configuration parsing function

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,9 @@
 import type { IConfig } from '@/types'
-import path, { resolve } from 'node:path'
-import process from 'node:process'
+import { resolve } from 'node:path'
 import { CACHE_TOKEN_FILE_PATH } from '@/constant.ts'
 
-export const resolveConfig = async (cwd: string): Promise<IConfig> => {
-    const cwdPath = cwd ? path.resolve(cwd) : process.cwd()
+export const resolveConfig = async (cwd: string = ''): Promise<IConfig> => {
+    const cwdPath = cwd ? resolve(cwd) : process.cwd()
     const tokenPath = resolve(cwdPath, `${CACHE_TOKEN_FILE_PATH}`)
 
     return {
