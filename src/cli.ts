bc3d06468d2e0de5e51075599aa617c112ac0b0a
lonewolfyx <877085893@qq.com>
2025-12-31 20:43:01 +0800

    chore(cli): Add release related TODO comments

--- src/cli.ts
+++ src/cli.ts
@@ -5,6 +5,9 @@ import { name, version } from '../package.json'
 
 const cli = cac(name)
 
+// TODO 完成主目录下的 package.json 版本发布
+// TODO 完成 monorepo 相关版本，多选版本发布
+
 cli.command('')
     .action(async (options) => {
         const config = await resolveConfig()
@@ -13,6 +16,9 @@ cli.command('')
         console.log(import.meta)
         // await setToken(config, 'asdasdasd')
         // console.log(await loaderToken(config))
+
+        // TODO runCommadn
+        // TODO ➜ npm publish --//registry.npmjs.org/:_authToken=${token} --access public
     })
 
 cli.help()
