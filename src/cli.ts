1e24006dd69865049a22d1651637320571c91d28
lonewolfyx <877085893@qq.com>
2026-01-03 23:52:43 +0800

    refactor(cli): Extract version upgrade logic into a separate command module

--- src/cli.ts
+++ src/cli.ts
@@ -1,14 +1,9 @@
 import * as process from 'node:process'
-import { confirm, intro, outro } from '@clack/prompts'
 import cac from 'cac'
-import pc from 'picocolors'
+import { bumpVersion } from '@/command/bump.ts'
 import { resolveConfig } from '@/config.ts'
-import { gitCommit, gitTags } from '@/git.ts'
 import { setToken } from '@/token.ts'
-import { updateFiles } from '@/update-files.ts'
-import { isCancelProcess, printWarning } from '@/utils.ts'
-import { getCurrentVersion } from '@/version/current.ts'
-import { promptForNewVersion } from '@/version/new.version'
+import { printWarning } from '@/utils.ts'
 import { name, version } from '../package.json'
 
 const cli = cac(name)
@@ -18,56 +13,7 @@ const cli = cac(name)
 
 cli.command('')
     .action(async (options) => {
-        const config = await resolveConfig()
-
-        intro(pc.bgCyan(` dnmp ${version} `))
-
-        if (config.monorepo.is) {
-            outro(pc.yellow('当前暂不支持 monorepo 的版本升级'))
-            process.exit(0)
-        }
-        // TODO 获取老的版本号
-        await getCurrentVersion(config)
-
-        // TODO 获取新的版本号
-        await promptForNewVersion(config)
-
-        console.log('npm', [
-            'publish',
-            `--//registry.npmjs.org/:_authToken=${config.token.value}`,
-            '--access',
-            'public',
-        ].join(' '))
-
-        const isConfirmUpdate = await confirm({
-            message: '是否确认更新 package.json ?',
-            initialValue: true,
-        })
-
-        isCancelProcess(isConfirmUpdate)
-
-        if (!isConfirmUpdate) {
-            outro('用户取消操作，后续进程停止')
-            return process.exit(0)
-        }
-
-        // TODO 更新 package.json 相应版本号
-        await updateFiles(config)
-
-        // TODO 提交 Git Commits release 信息
-        await gitCommit(config)
-        // TODO 创建 Git tag
-        await gitTags(config)
-        // TODO 发布 NPM
-
-        // await runCommand(config, 'npm', [
-        //     'publish',
-        //     `--//registry.npmjs.org/:_authToken=${config.token}`,
-        //     '--access',
-        //     'public',
-        // ])
-
-        outro('Done.')
+        await bumpVersion()
     })
 
 cli.command('set <token>', 'Set the local release Token')
---
+++ src/command/bump.ts
@@ -0,0 +1,63 @@
+import process from 'node:process'
+import { confirm, intro, outro } from '@clack/prompts'
+import pc from 'picocolors'
+import { resolveConfig } from '@/config.ts'
+import { gitCommit, gitTags } from '@/git.ts'
+import { updateFiles } from '@/update-files.ts'
+import { isCancelProcess } from '@/utils.ts'
+import { getCurrentVersion } from '@/version/current.ts'
+import { promptForNewVersion } from '@/version/new.version.ts'
+import { version } from '../../package.json'
+
+export const bumpVersion = async () => {
+    const config = await resolveConfig()
+
+    intro(pc.bgCyan(` dnmp ${version} `))
+
+    if (config.monorepo.is) {
+        outro(pc.yellow('当前暂不支持 monorepo 的版本升级'))
+        process.exit(0)
+    }
+    // TODO 获取老的版本号
+    await getCurrentVersion(config)
+
+    // TODO 获取新的版本号
+    await promptForNewVersion(config)
+
+    console.log('npm', [
+        'publish',
+        `--//registry.npmjs.org/:_authToken=${config.token.value}`,
+        '--access',
+        'public',
+    ].join(' '))
+
+    const isConfirmUpdate = await confirm({
+        message: '是否确认更新 package.json ?',
+        initialValue: true,
+    })
+
+    isCancelProcess(isConfirmUpdate)
+
+    if (!isConfirmUpdate) {
+        outro('用户取消操作，后续进程停止')
+        return process.exit(0)
+    }
+
+    // TODO 更新 package.json 相应版本号
+    await updateFiles(config)
+
+    // TODO 提交 Git Commits release 信息
+    await gitCommit(config)
+    // TODO 创建 Git tag
+    await gitTags(config)
+    // TODO 发布 NPM
+
+    // await runCommand(config, 'npm', [
+    //     'publish',
+    //     `--//registry.npmjs.org/:_authToken=${config.token}`,
+    //     '--access',
+    //     'public',
+    // ])
+
+    outro('Done.')
+}
