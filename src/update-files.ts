2992e5aef6c38717b6f2b730f74292875a1fff67
lonewolfyx <877085893@qq.com>
2026-01-04 21:35:52 +0800

    fix(update-files): Remove unnecessary type assertions

--- src/update-files.ts
+++ src/update-files.ts
@@ -2,7 +2,7 @@ import type { IConfigOptions } from '@/types'
 import { readFile, writeFile } from 'node:fs/promises'
 
 export const updateFiles = async (config: IConfigOptions) => {
-    const updatePackage = JSON.parse(await readFile(config.packages as string, 'utf-8'))
+    const updatePackage = JSON.parse(await readFile(config.packages, 'utf-8'))
     updatePackage.version = config.release as string
-    await writeFile(config.packages as string, JSON.stringify(updatePackage, null, 2))
+    await writeFile(config.packages, JSON.stringify(updatePackage, null, 2))
 }
