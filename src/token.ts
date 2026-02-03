1f9e4967c180677c962b55da687f5f6656e8a6df
lonewolfyx <877085893@qq.com>
2026-02-03 22:14:25 +0800

    fix(token): update token loading to read from .npmrc file

--- src/token.ts
+++ src/token.ts
@@ -1,8 +1,11 @@
 import type { IConfigOptions } from '@/types'
-import { writeFile } from 'node:fs/promises'
+import { readFile, writeFile } from 'node:fs/promises'
 import { resolve } from 'node:path'
+import process from 'node:process'
+import boxen from 'boxen'
+import { x } from 'tinyexec'
 import { CACHE_FOLDER_NAME } from '@/constant.ts'
-import { createDir, loaderTs } from '@/utils.ts'
+import { createDir } from '@/utils.ts'
 
 export const setToken = async (config: IConfigOptions, token: string): Promise<void> => {
     createDir(resolve(config.cwd, CACHE_FOLDER_NAME))
@@ -10,5 +13,30 @@ export const setToken = async (config: IConfigOptions, token: string): Promise<v
 }
 
 export const loaderToken = async (config: IConfigOptions): Promise<string> => {
-    return await loaderTs(config.token.file)
+    const npmrcPath = await x('npm', ['config', 'get', 'userconfig'], {
+        nodeOptions: {
+            cwd: config.cwd,
+            stdio: 'pipe',
+        },
+    })
+
+    const content = await readFile(npmrcPath.stdout.trim(), 'utf-8')
+    const [, tokenValue] = content.match(/\/\/registry.npmjs.org\/:_authToken=(.*)/) || []
+    const token = tokenValue?.trim() || null
+    if (!token) {
+        console.log(boxen(
+            `_authToken not found in your .npmrc file,
+you can use the following codemod:
+Run "dnmp set <token> or npm config set //registry.npmjs.org/:_authToken=<token>"`,
+            {
+                title: 'Warning',
+                padding: 1,
+                margin: 0,
+                borderStyle: 'round',
+                borderColor: 'yellow',
+            },
+        ))
+        process.exit(0)
+    }
+    return token
 }
